57937b619a9df1823a52edfd6802c1a8952e4bd9
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-09-01 18:09:45 +0700

    upload grafana dashboard

--- web-app/src/services/authenticationService.js
+++ web-app/src/services/authenticationService.js
@@ -1,15 +1,12 @@
-import {getToken, removeToken, setToken} from "./localStorageService";
+import { getToken, removeToken, setToken } from "./localStorageService";
 import httpClient from "../configurations/httpClient";
-import {API} from "../configurations/configuration";
+import { API } from "../configurations/configuration";
 
 export const logIn = async (username, password) => {
-    let response = null;
-    for (let i = 0; i < 100; i++) {
-        response = await httpClient.post(API.LOGIN, {
-            username: username,
-            password: password,
-        });
-    }
+    const response = await httpClient.post(API.LOGIN, {
+        username: username,
+        password: password,
+    });
 
     setToken(response.data?.result?.token);
 
