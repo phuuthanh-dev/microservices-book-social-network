c263ffe8e726c5ccb48bfe4352ec4b22c7f44756
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-09-05 00:44:31 +0700

    update api-gateway and docker-compose

--- identity-service/src/main/java/com/booksn/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/booksn/identity/repository/httpclient/ProfileClient.java
@@ -10,7 +10,8 @@ import com.booksn.identity.dto.ApiResponse;
 import com.booksn.identity.dto.request.ProfileCreationRequest;
 import com.booksn.identity.dto.response.UserProfileResponse;
 
-@FeignClient(name = "profile-service",
+@FeignClient(
+        name = "profile-service",
         configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
     @PostMapping(value = "/profile/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
